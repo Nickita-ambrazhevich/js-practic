17276bf9fff3e2624027fc0e8c215b89d4f10327
nickita ambrazhevich <nickita.ambrazhevich@gmail.com>
2023-04-28 22:43:29 +0300

    fix lesson-1

--- lesson-1/js/script.js
+++ lesson-1/js/script.js
@@ -1,21 +1,21 @@
 let name = prompt( 'Ваше имя?')
-let weight = + prompt( 'Ваш вес?')
-let height = + prompt( 'Ваш рост в метрах ?')
+let weight =  prompt( 'Ваш вес?')
+let height =  prompt( 'Ваш рост в метрах ?')
 
 
-let index =  weight /  height ** 2;
+let bodyIndex =  weight /  height ** 2;
 
-if (index <= 16.5){ 
-alert(name + " У вас крайний недостаток веса. Ваш ИМТ =" + index);
+if (bodyIndex  <= 16.5){ 
+alert(name + " У вас крайний недостаток веса. Ваш ИМТ =" + bodyIndex );
 }
-else if ( 16.5 < index && index < 18.5) { 
-alert(name + " У вас недостаток веса. Ваш ИМТ =" + index );
+else if ( bodyIndex  < 18.5) { 
+alert(name + " У вас недостаток веса. Ваш ИМТ =" + bodyIndex  );
 }
-else if ( 18.5 < index && index < 24.9) { 
-alert(name + " У вас нормальный вес тела .Ваш ИМТ =" + index);
+else if ( bodyIndex < 24.9) { 
+alert(name + " У вас нормальный вес тела .Ваш ИМТ =" + bodyIndex );
 }
-else if ( 25 < index && index < 30) { 
-alert(name + " У вас избытончый вес тела  Ваш ИМТ =" + index);
+else if ( bodyIndex  < 30) { 
+alert(name + " У вас избытончый вес тела  Ваш ИМТ =" + bodyIndex );
 };
 
 alert("Спасибо что воспользовались нашим сервисом!")
