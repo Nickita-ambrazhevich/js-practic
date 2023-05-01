2be007003ff6a44201622c7c70670bd7c9d2164a
nickita ambrazhevich <nickita.ambrazhevich@gmail.com>
2023-05-01 20:16:24 +0300

    fix lesson-2

--- lesson-2/js/script.js
+++ lesson-2/js/script.js
@@ -1,10 +1,17 @@
-const arr = [
-    [1, 5, 6],
-    [7, 2, 4, 6],
-    [7, 10, 1],
-    [1, 3]
-]
-const sum = arr.reduce(function (acc, item, imdex) {
-    return acc + item + index
-  })
-  console.log(sum)
+const arr = [ 5, 7, 
+    [ 4, [2], 8, [1,3], 2 ], 
+    [ 9, [] ], 
+    1, 8
+  ]
+var sum = 0;
+
+function foo(array) {
+
+  array.forEach(function(value, index) {
+    Array.isArray(value) ? foo(value) : sum += value; 
+  });
+
+  return sum;
+}
+
+console.log(foo(arr));
