22eb7fb4b2f4de36d567aea3148d0e84cd7ccf5f
nickita ambrazhevich <nickita.ambrazhevich@gmail.com>
2023-05-01 18:11:19 +0300

    add lesson-2

---
+++ lesson-2/js/script.js
@@ -0,0 +1,10 @@
+const arr = [
+    [1, 5, 6],
+    [7, 2, 4, 6],
+    [7, 10, 1],
+    [1, 3]
+]
+const sum = arr.reduce(function (acc, item, imdex) {
+    return acc + item + index
+  })
+  console.log(sum)
