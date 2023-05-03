62e61fcf4aa5e8657a2334aa3514c4ec8026a25e
nickita ambrazhevich <nickita.ambrazhevich@gmail.com>
2023-05-03 17:26:29 +0300

    fix lesson-4

--- lesson-4/js/script.js
+++ lesson-4/js/script.js
@@ -4,14 +4,19 @@ function randomDiap(n,m) {
 
 function mood(colorsCount) {
 const colors=[ '', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый' ];
-
+let shuffledColor = {};
 console.log( 'цветов: ' + colorsCount );
 for ( let i=1; i<=colorsCount; i++ ) {
     const n = randomDiap(1,7);
     const colorName=colors[n];
-    let shuffledColors= []
     console.log( colorName );
+    shuffledColor. addColor = colorName;
+console.log(shuffledColor);
+ 
+    return colorsCount;
+
 }
 }
+
 
 mood(3);
